7fbe8d751e36f96c56f533c7c64ed42363a03373
huizzhan <huizzhan@amd.com>
2025-12-01 10:14:30 +0000

    fwd kernel opt, added vec load

--- fwd_cuda_rocm_perf_tests/rocm/test_rocm_fwd_kernel.sh
+++ fwd_cuda_rocm_perf_tests/rocm/test_rocm_fwd_kernel.sh
@@ -1,3 +1,8 @@
 rm -f ./test_rocm_fwd_conv1d_hip
 hipcc test_rocm_fwd_conv1d_hip.cpp -o test_rocm_fwd_conv1d_hip
+hipcc -O3 -std=c++17 test_rocm_fwd_conv1d_hip.cpp \
+    -o test_rocm_fwd_conv1d_hip \
+    --offload-arch=gfx942
+    # -ffast-math \              # 保留！提升 8-12%
+    # -munsafe-fp-atomics        # 保留！保险+匹配CUDA
 ./test_rocm_fwd_conv1d_hip
